fd45803adec04178b3bd397393bd8d48f46e6d06
1126571387 <1126571387@qq.com>
2020-03-19 07:28:13 +0800

    服务

---
+++ public/precache-manifest.8a38391ea6a5da948eca6f9f92a62140.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131a74d5e4e27aeaa3a62028524f8960",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f3958907db458bd1dec",
+    "url": "/static/css/2.b5da44c3.chunk.css"
+  },
+  {
+    "revision": "1e3318048030ae1afc11",
+    "url": "/static/css/main.6c2f945e.chunk.css"
+  },
+  {
+    "revision": "9f3958907db458bd1dec",
+    "url": "/static/js/2.14fed19a.chunk.js"
+  },
+  {
+    "revision": "1e3318048030ae1afc11",
+    "url": "/static/js/main.ccc3debe.chunk.js"
+  },
+  {
+    "revision": "e0e52ab8caa45658c811",
+    "url": "/static/js/runtime-main.bdaa9917.js"
+  },
+  {
+    "revision": "9161e238b798c444b732dbbb7420a09e",
+    "url": "/static/media/404.9161e238.png"
+  },
+  {
+    "revision": "ab45504baf89be7c7792b4426c4a6594",
+    "url": "/static/media/bg.ab45504b.jpg"
+  },
+  {
+    "revision": "ba1f87ecbc18b2f0a8b6e29e8676ac82",
+    "url": "/static/media/logo.ba1f87ec.png"
+  }
+]);
